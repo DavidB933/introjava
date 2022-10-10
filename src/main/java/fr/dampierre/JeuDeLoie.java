4c294a6aa727ef7ec5a8790c9435208018165b3f
DavidB933 <benidavid75@gmail.com>
2022-10-10 22:11:45 +0200

    Jeu de l'oie - J'ai du mal à comprendre l'exercice

---
+++ src/main/java/fr/dampierre/JeuDeLoie.java
@@ -0,0 +1,14 @@
+package fr.dampierre;
+import java.util.Scanner;
+public class JeuDeLoie {
+    public static void main(String[] args) {
+int nbCases = 20 ; 
+int nbDeFacesDe = 6 ; 
+
+Random generateur = new Random();  
+int nbDeLanceeDe =  generateur.nextInt(6) ; 
+
+
+
+}
+}
