4f60b7aaa9f4f0419a9ee407d3b1037c89cc3cf5
DavidB933 <benidavid75@gmail.com>
2022-10-13 10:05:47 +0200

    TP3 Suite 2 Mal programmé

---
+++ src/main/java/fr/dampierre/Bouclefor2.java
@@ -0,0 +1,10 @@
+package fr.dampierre;
+
+public class Bouclefor2 {
+    public static void main(String[] args) {
+        for (int i = 0; i<=100; i=i+1 ) {
+            i = i^2;
+            System.out.println(i); 
+        }
+}
+}
