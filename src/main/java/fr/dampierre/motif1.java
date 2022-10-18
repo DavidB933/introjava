f4aec77d649e265e2c28b61faa30c30c9da76d8b
DavidB933 <benidavid75@gmail.com>
2022-10-18 11:02:16 +0200

    Motif1 Réussi

---
+++ src/main/java/fr/dampierre/motif1.java
@@ -0,0 +1,33 @@
+package fr.dampierre;
+
+public class motif1 {
+    public static void main(String[] args) {
+        for (int i=1; i<41; i=i+1) {
+            System.out.print("-");
+        }
+        System.out.println();   //Boucle for pour afficher les "-"//
+
+        for (int i=1; i<9; i=i+1) {
+            System.out.print("_-^-_");    // Boucle of for pour les caractères "_-^-_"//
+        } 
+        System.out.println();
+
+    for (int i=1; i<=9; i=i+1) {
+        System.out.print(i); System.out.print(i); 
+    }
+    System.out.print("00"); //Boucle for pour afficher les caractères de 1 à 9 et les 0//
+
+    for (int i=1; i<=9; i=i+1) {
+        System.out.print(i); System.out.print(i); //Boucle for pour afficher les caractères de 1 à 9 et les 0//
+    }
+    System.out.print("00");
+    System.out.println();
+
+    for (int i=1; i<41; i=i+1) {
+        System.out.print("-");
+    }       //Boucle for pour afficher les "-"//
+
+    
+
+}
+}
