d144987093e85b9029c44f32ce6345a7b02dc384
DavidB933 <benidavid75@gmail.com>
2022-10-18 11:09:41 +0200

    Motif 2 Réussi

---
+++ src/main/java/fr/dampierre/motif2.java
@@ -0,0 +1,26 @@
+package fr.dampierre;
+
+public class motif2 {
+    public static void main(String[] args) {
+        for (int i=1; i<=5; i=i+1) {
+            System.out.print("*");
+        }
+        System.out.println(); 
+
+        for (int i=1; i<=5; i=i+1) {
+            System.out.print("*");
+        }
+        System.out.println();
+
+        for (int i=1; i<=5; i=i+1) {
+            System.out.print("*");
+        }
+        System.out.println();
+
+        for (int i=1; i<=5; i=i+1) {
+            System.out.print("*");
+        }
+
+        
+}
+}
