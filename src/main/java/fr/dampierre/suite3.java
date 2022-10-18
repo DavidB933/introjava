35ae0ca04f108b3a8ec7d10c0ffa3318db13c4ef
DavidB933 <benidavid75@gmail.com>
2022-10-18 09:06:01 +0200

    Suite 3 complétée avec le prof.

---
+++ src/main/java/fr/dampierre/suite3.java
@@ -0,0 +1,11 @@
+package fr.dampierre;
+
+public class suite3 {
+    public static void main(String[] args) {
+
+    for (int i=1; i<101; i=i+1){
+        System.out.print(i * i+ " ");
+    }
+
+}
+}
