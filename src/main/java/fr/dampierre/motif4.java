ede10368088abb09ccbc6f39e1418f56792e07a8
DavidB933 <benidavid75@gmail.com>
2022-10-20 12:05:27 +0200

    Motif4 - David et Exaucé sur le même PC.

---
+++ src/main/java/fr/dampierre/motif4.java
@@ -0,0 +1,35 @@
+package fr.dampierre;
+
+public class motif4 {
+    public static void main(String[] args) {
+        for (int i=1; i==1;i=i+1) {
+            System.out.println(i);
+        }
+        for (int i=2; i==2; i=i+1) {
+            System.out.print(i); System.out.print(i);
+        }
+        System.out.println();
+
+        for (int i=3; i==3; i=i+1) {
+            System.out.print(i); System.out.print(i); System.out.print(i);
+        }
+        System.out.println(); 
+        for (int i=4; i==4; i=i+1) {
+            System.out.print(i); System.out.print(i); System.out.print(i);
+            System.out.print(i); 
+        }
+        System.out.println(); 
+        for (int i=5; i==5; i=i+1) {
+            System.out.print(i); System.out.print(i); System.out.print(i);
+            System.out.print(i); System.out.print(i);  
+        }
+        System.out.println(); 
+        for (int i=6; i==6; i=i+1) {
+            System.out.print(i); System.out.print(i); System.out.print(i);
+            System.out.print(i); System.out.print(i); System.out.print(i);   
+        }
+
+
+
+}
+}
