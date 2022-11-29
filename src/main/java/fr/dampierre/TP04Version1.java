93b324a00bfa05c9d7acf020ec0671352da5a424
DavidB933 <benidavid75@gmail.com>
2022-11-29 09:51:20 +0100

    TP04Version01 - David Beni V2

--- src/main/java/fr/dampierre/TP04Version1.java
+++ src/main/java/fr/dampierre/TP04Version1.java
@@ -5,22 +5,22 @@ import java.util.Scanner;
 
 public class TP04Version1 {
     public static void main(String[] args) {
-        int nbEntre = GenererUnNombre(); 
-        int nbPense = EntrerUnNombre(); 
-        System.out.println("Vous proposez : " + nbPense);
-        System.out.println("Le nombre auquel je pensais étais : " + nbEntre); 
-        int difference = nbEntre - nbPense; 
+        int nbEntre = EntrerUnNombre(); 
+        int nbPense = GenererUnNombre(); 
+        System.out.println("Vous proposez : " + nbEntre);
+        System.out.println("Le nombre auquel je pensais étais : " + nbPense); 
+        int difference = nbPense - nbEntre; 
         System.out.println("Vous étiez à " + difference); 
     }
      
     public static int GenererUnNombre () {
-        System.out.println("Je pense à un nombre entre 1 et 100 inclus. Devinez lequel."); 
         Random generateur = new Random(); 
         int nbPense = generateur.nextInt(100)+1; 
         return nbPense; 
     }
 
     public static int EntrerUnNombre () {
+        System.out.println("Je pense à un nombre entre 1 et 100 inclus. Devinez lequel.");
         System.out.print("Entrez un nombre : "); 
         Scanner clavier = new Scanner(System.in); 
         int nbEntre = clavier.nextInt(); 
