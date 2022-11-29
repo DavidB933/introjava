a13f1a6d3d9b6a3b9026a505711c36312f55fe4c
DavidB933 <benidavid75@gmail.com>
2022-11-29 09:47:51 +0100

    TP04Version02 -  David Beni V2

--- src/main/java/fr/dampierre/TP04Version02.java
+++ src/main/java/fr/dampierre/TP04Version02.java
@@ -5,15 +5,16 @@ import java.util.Scanner;
 
 public class TP04Version02 {
     public static void main(String[] args) {
-        int nbEntre = GenererUnNombre(); 
-        int nbPense = EntrerUnNombre(); 
-        System.out.println("Vous proposez : " + nbPense); 
-        System.out.println("Le nombre auquel je pensais étais : " + nbEntre); 
-        int difference = nbEntre - nbPense; 
+        int nbEntre = EntrerUnNombre(); 
+        int nbPense = GenererUnNombre(); 
+        System.out.println("Vous proposez : " + nbEntre);
+        System.out.println("Le nombre auquel je pensais étais : " + nbPense); 
+        int difference = (nbPense - nbEntre) ; 
+        int depassement = (nbEntre - nbPense) ; 
         if (nbEntre > nbPense) {
-            System.out.println("Perdu ! Vous avez dépassé de " + ((nbPense - nbEntre) * -1));
+            System.out.println("Perdu ! Vous avez dépassé de " + depassement);
         } else if (nbEntre < nbPense) {
-            System.out.println("Perdu ! Il vous manquait " + (difference * -1)); // nbEntre-nbPense c'est la différence//
+            System.out.println("Perdu ! Il vous manquait " + difference);
         } else {
             System.out.println("Quel bol, vous avez trouvé !"); 
         }
@@ -21,13 +22,13 @@ public class TP04Version02 {
     }
      
     public static int GenererUnNombre () {
-        System.out.println("Je pense à un nombre entre 1 et 100 inclus. Devinez lequel."); 
         Random generateur = new Random(); 
         int nbPense = generateur.nextInt(100)+1; 
         return nbPense; 
     }
 
     public static int EntrerUnNombre () {
+        System.out.println("Je pense à un nombre entre 1 et 100 inclus. Devinez lequel.");
         System.out.print("Entrez un nombre : "); 
         Scanner clavier = new Scanner(System.in); 
         int nbEntre = clavier.nextInt(); 
---
+++ src/main/java/fr/dampierre/TP04Version03.java
@@ -0,0 +1,8 @@
+package fr.dampierre;
+
+public class TP04Version03 {
+    public static void Main () {
+        
+    }
+    
+}
